85bd444c81958ad2f0bff4b939efd130bda4bb9d
William Martinez <wrmartinez709@gmail.com>
2023-02-08 22:43:20 -0500

    Complete input/output exercises

---
+++ 03-input-output/greeter.js
@@ -0,0 +1,11 @@
+// 1. Write a dynamic greeter program named greeter.js. The program should ask for your name, then output Hello, {name}! where {name} is the name you entered
+let rlSync = require("readline-sync");
+
+// let name = rlSync.question("What is your name?\n");
+// console.log(`Hello, ${name}!`);
+
+// 2. Modify the greeter.js program to ask for the user's first and last names separately, then greet the user with their full name.
+let firstName = rlSync.question("What is your first name?\n");
+let lastName = rlSync.question("What is your last name?\n")
+
+console.log(`Hello, ${firstName} ${lastName}!`);
